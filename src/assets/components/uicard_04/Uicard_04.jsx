312fc1876ecde24d41bd4538b816e17d50cb8ea9
jovination <jovination791@gmail.com>
2024-04-12 22:38:25 +0300

    addition style to search cat

--- src/assets/components/uicard_04/Uicard_04.jsx
+++ src/assets/components/uicard_04/Uicard_04.jsx
@@ -12,7 +12,14 @@ function Uicard_04() {
             <img className='search_icon' src={SearchIcon} alt="" />
           </button>
         </form>
-        <div className="search_cat"></div>
+        <div className="search_cat">
+          <ul>
+            <li className='link'><a href="">Politics</a></li>
+            <li className='link'><a href="">Science</a></li>
+            <li className='link'><a href="">Movie</a></li>
+            <li className='link'><a href="">Technology</a></li>
+          </ul>
+        </div>
       </div>
     </div>
   )
