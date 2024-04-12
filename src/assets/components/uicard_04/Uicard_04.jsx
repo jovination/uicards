0aa9d361b4d9ed88aa5812af920aab2bb38e1093
jovination <jovination791@gmail.com>
2024-04-12 21:42:43 +0300

    few changes

--- src/assets/components/uicard_04/Uicard_04.jsx
+++ src/assets/components/uicard_04/Uicard_04.jsx
@@ -1,4 +1,5 @@
 import './uicard_04.css'
+import SearchIcon from '../../../../public/search.svg';
 import React from 'react'
 
 function Uicard_04() {
@@ -7,7 +8,9 @@ function Uicard_04() {
       <div className="form_ui">
         <form action="" className="search_bar">
           <input type="text" name="" id="search_input" placeholder='Search for articles'/>
-          <button></button>
+          <button className='search-btn'>
+            <img className='search_icon' src={SearchIcon} alt="" />
+          </button>
         </form>
         <div className="search_cat"></div>
       </div>
