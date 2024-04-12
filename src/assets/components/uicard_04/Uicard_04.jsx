566ae6a1e20049a9887598f81a8d1de61f460e99
jovination <jovination791@gmail.com>
2024-04-12 20:59:46 +0300

    few changes

--- src/assets/components/uicard_04/Uicard_04.jsx
+++ src/assets/components/uicard_04/Uicard_04.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 
 function Uicard_04() {
   return (
-    <div className="ui_wrapper_04"></div>
+    <div className="ui_wrapper_04">
+      <div className="form_ui">
+        <form action="" className="search_bar">
+          <input type="text" name="" id="search_input" placeholder='Search for articles'/>
+          <button></button>
+        </form>
+        <div className="search_cat"></div>
+      </div>
+    </div>
   )
 }
 
